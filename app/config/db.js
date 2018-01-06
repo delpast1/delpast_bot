66d80560abb8733d49bf6a54f050dde3b05af98b
delpast1 <lamquanguit@gmail.com>
2018-01-07 01:32:12 +0700

    change to mlab google

--- app/config/db.js
+++ app/config/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://delpast1:123456@ds048368.mlab.com:48368/bittrex');
+mongoose.connect('mongodb://delpast1:123456@ds143907.mlab.com:43907/coinmarketcap');
 
 exports = module.exports = mongoose;
--- app/controllers/ticker.js
+++ app/controllers/ticker.js
@@ -5,11 +5,13 @@ const axios = require('axios');
 const bot = require('../config/bot');
 
 var addTickers = (req, res) => {
-    console.log('check');
+    
     axios
-        .get(`https://api.coinmarketcap.com/v1/ticker/?limit=1385`)
+        .get(`https://api.coinmarketcap.com/v1/ticker/?limit=1384`)
         .then(res => {
+            
             const data = res.data;
+            console.log(data[0]);
             for(let i=0; i< data.length; i++) {
                 var newTicker = new Ticker();
                 newTicker.name = data[i].id;
